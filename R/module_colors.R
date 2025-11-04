672c2755c1f8ec873990b75c8a71ea268cbcfc3a
Nicola Palmieri <nicola-palmieri@Mac-Studio.local>
2025-11-04 14:16:11 +0100

    improved reactivity in module colors

--- R/module_colors.R
+++ R/module_colors.R
@@ -12,60 +12,53 @@ add_color_customization_ui <- function(ns, multi_group = TRUE) {
 
 # ---- SERVER ----
 add_color_customization_server <- function(ns, input, output, data, color_var_reactive, multi_group = TRUE) {
+  default_color <- "steelblue"
+  
+  # ---- Dynamic UI ----
   output$color_custom_ui <- renderUI({
     req(data())
-    color_var <- color_var_reactive()
-
-    single_color_ui <- tagList(
-      br(),
-      h5("Line color"),
-      color_dropdown_input(ns, "single_color", basic_color_palette, ncol = 4)
-    )
-
-    if (isTRUE(multi_group)) {
-      if (is.null(color_var) || identical(color_var, "") || identical(color_var, "None")) {
-        single_color_ui
-      } else {
-        render_color_inputs(ns, data, color_var)
-      }
+    
+    color_var <- color_var_reactive() %||% ""
+    
+    # Single color UI shown when multi-group off or no color_var available
+    if (!isTRUE(multi_group) || color_var %in% c("", "None")) {
+      tagList(
+        br(),
+        h5("Line color"),
+        color_dropdown_input(ns, "single_color", basic_color_palette, ncol = 4)
+      )
     } else {
-      single_color_ui
+      render_color_inputs(ns, data, color_var)
     }
   })
-
+  
+  # ---- Reactive color mapping ----
   reactive({
-    if (isTRUE(multi_group)) {
-      color_var <- color_var_reactive()
-      if (is.null(color_var) || identical(color_var, "") || identical(color_var, "None")) {
-        selected_color <- input$single_color
-        if (is.null(selected_color) || identical(selected_color, "")) selected_color <- "steelblue"
-        return(selected_color)
-      }
-
-      dataset <- data()
-      if (is.null(dataset) || !color_var %in% names(dataset)) {
-        selected_color <- input$single_color
-        if (is.null(selected_color) || identical(selected_color, "")) selected_color <- "steelblue"
-        return(selected_color)
-      }
-
-      lvls <- levels(as.factor(dataset[[color_var]]))
-      base_palette <- rep(basic_color_palette, length.out = length(lvls))
-      cols <- vapply(seq_along(lvls), function(i) {
-        input_val <- input[[paste0("col_", color_var, "_", i)]]
-        if (is.null(input_val) || identical(input_val, "")) {
-          base_palette[i]
-        } else {
-          input_val
-        }
-      }, character(1))
-      names(cols) <- lvls
-      cols
-    } else {
-      selected_color <- input$single_color
-      if (is.null(selected_color)) selected_color <- "steelblue"
-      selected_color
+    # --- Single-color mode (no grouping variable or disabled) ---
+    if (!isTRUE(multi_group)) {
+      return(input$single_color %||% default_color)
     }
+    
+    color_var <- color_var_reactive() %||% ""
+    if (color_var %in% c("", "None")) {
+      return(input$single_color %||% default_color)
+    }
+    
+    dataset <- data()
+    req(dataset)
+    
+    if (!color_var %in% names(dataset)) {
+      return(input$single_color %||% default_color)
+    }
+    
+    lvls <- levels(as.factor(dataset[[color_var]]))
+    base_palette <- rep(basic_color_palette, length.out = length(lvls))
+    
+    cols <- vapply(seq_along(lvls), function(i) {
+      input[[paste0("col_", color_var, "_", i)]] %||% base_palette[i]
+    }, character(1))
+    
+    names(cols) <- lvls
+    cols
   })
 }
-
